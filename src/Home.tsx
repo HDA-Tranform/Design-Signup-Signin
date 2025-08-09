4d6e280776fd3155a55f62832ec3a5a2d5c9d8ea
Hà Đức An <22H1120001@ut.edu.vn>
2025-08-09 15:48:00 +0700

    add new information

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,2 @@
-<div>xin chao the gioi</div>
-<<<<<<< HEAD
-<div>Code web ae ơi, cố lên!</div>
-=======
-<div>Code web ae ơi,</div>
->>>>>>> ab8b35738215d93d81f25329d4fc2e19e149872e
+<p>xin chao the gioi</p>
+<p>Code web ae ơi, cố lên!</p>
